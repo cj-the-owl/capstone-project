5812e56313ae0d49c17f031ff63874c2cd2832fe
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-08-31 09:58:54 +0200

    mql connection

---
+++ api/config/db.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const {createPool} = require("mysql")
+
+const connection = createPool({
+    host: process.env.dbHost,
+    database: process.env.dbDatabase,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    port: process.env.dbPort,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+module.exports = connection
