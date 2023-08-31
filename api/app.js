172ec5f2604c0997f95870a747cdface6c437bfe
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-08-31 09:34:36 +0200

    file structure and app.js done

---
+++ api/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+// const { errorHandling } = require("./middleware")
+// const route = require("./controller")
+
+app.use((req, res, next) => {
+    res.header("Acess-Control-Allow-Origin", "*",)
+    res.header("Acess-Control-Allow_Credentials", "true",)
+    res.header("Acess-Control-Allow-Methods", "*",)
+    res.header("Acess-Control-Allow-Headers", "*",)
+    next()
+})
+
+app.use(
+    cors(),
+    cookieParser(),
+    express.json(),
+    express.urlencoded({ extended: false })
+)
+
+// app.use(route)
+// app.use(errorHandling)
+
+app.listen(port, () => {
+    console.log(`Server is vibing on PORT: ${port}`)
+})
