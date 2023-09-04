3b14af3a2a83ffe5345af331a52ee3be07604847
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-09-04 14:02:34 +0200

    footer done completely

--- capstone-frontend/src/components/footerComp.vue
+++ capstone-frontend/src/components/footerComp.vue
@@ -3,23 +3,23 @@
         <div class="footer-info">
             <ul class="me-auto">
                 <li class="fw-bold">Bookworm.Co</li>
-                <li>Address</li><br>
-                <li>Phone</li><br>
-                <li>Email</li>
+                <li>314 Imam Haron Road Lansdowne</li><br>
+                <li>0712345678</li><br>
+                <li>lodewykcarynjayn@gmail.com</li>
             </ul>
             <ul class="social">
                 <li class="fw-bold">Socials</li>
-                <li>Instagram</li><br>
-                <li>LinkedIn</li><br>
-                <li>Facebook</li>
+                <a href="https://www.instagram.com/cj.the.owl/"><li class="soc">Instagram</li></a><br>
+                <a href="https://www.linkedin.com/in/caryn-jayn-lodewyk-411165276/"><li class="soc">LinkedIn</li></a><br>
+                <a href="https://www.facebook.com/carynjayn.lodewyk"><li class="soc">Facebook</li></a>
             </ul>
             <ul class="social">
                 <li class="fw-bold">Navigation:</li>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Our Books</li>
-                <li>Contact Us</li>
-                <li>Admin</li>
+                <router-link to="/"><li class="navi">Home</li></router-link> 
+                <router-link to="/about"><li class="navi">About Us</li></router-link>
+                <li class="navi">Our Books</li>
+                <router-link to="/contact"><li class="navi">Contact Us</li></router-link>
+                <li class="navi">Admin</li>
             </ul>
             <ul class="social">
                 <li class="fw-bold">Business Hours:</li>
@@ -56,6 +56,18 @@
 
     .social {
         padding-right: 20px;
+    }
+
+    li {
+        color: black;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .navi:hover, .soc:hover {
+        color: #FFE280;
     }
 
     .footer-info {
