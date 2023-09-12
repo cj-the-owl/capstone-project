59b94a561d1863b05a10ee3db215d48983a7d9fe
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-09-12 14:13:08 +0200

    routes set up

--- api/controller/index.js
+++ api/controller/index.js
@@ -6,11 +6,13 @@ const {Products} = require("../model/products")
 const {User} = require("../model/users")
 const product = new Products()
 const user = new User()
+const cart = new Cart()
 
 //for the home page of the api
 route.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../static/html/index.html'));
 });
+
 
 route.get('/products', (req, res) => {
     product.fetchProducts(req, res);
@@ -56,5 +58,26 @@ route.post("/login", bodyParser.json(), (req, res) => {
 
 route.post("/register", bodyParser.json(), (req, res) => {
     user.register(req, res)
+})
+
+
+route.get('/user/:id/carts', (req, res) => {
+    cart.fetchCart(req, res);
+})
+
+route.post('/user/:id/cart', bodyParser.json(), (req, res) => {
+    cart.addCart(req, res);
+})
+
+route.put('/user/:id/cart/:id', bodyParser.json(), (req, res) => {
+    cart.updateCart(req, res);
+})
+
+route.delete('/user/:id/cart', (req, res) => {
+    cart.deleteCart(req, res);
+})
+
+route.delete('/user/:id/cart/:id', (req, res) => {
+    cart.deleteACart(req, res);
 })
 module.exports = route
