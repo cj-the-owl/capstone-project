f03a764fd3abd59be5c5d1239cdc3e6281835fc5
cj-the-owl <130046870+cj-the-owl@users.noreply.github.com>
2023-09-15 11:22:47 +0200

    took out app.use

--- api/app.js
+++ api/app.js
@@ -7,7 +7,6 @@ const cookieParser = require("cookie-parser")
 const { errorHandling } = require("./middleware/errorHandling")
 const route = require("./controller")
 
-app.use(express.static(__dirname + '/static'));
 
 app.use((req, res, next) => {
     res.header("Acess-Control-Allow-Origin", "*",)
